7dd3141e6a3a243172a440aa38f224fdb3235087
swordfatih <accfldekur@gmail.com>
2022-03-29 21:36:53 +0200

    fix libs

--- packages/o/oof/xmake.lua
+++ packages/o/oof/xmake.lua
@@ -4,7 +4,7 @@ package("oof")
     set_description("Convenient, high-performance RGB color and position control for console output")
 
     add_urls("https://github.com/s9w/oof/archive/refs/tags/second.tar.gz")
-    add_versions("second", "a8bf39e9e648ca34a85cd15d2b989d713b657bebc0674bde0c3594863c6da34b")
+    add_versions("latest", "a8bf39e9e648ca34a85cd15d2b989d713b657bebc0674bde0c3594863c6da34b")
 
     on_install(function (package)
         os.cp("oof.h", package:installdir("include"))
