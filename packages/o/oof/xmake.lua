2d8b366e368ff6796e83a603196c8293a3a6af84
swordfatih <accfldekur@gmail.com>
2022-03-29 21:06:36 +0200

    fix libs

--- packages/o/oof/xmake.lua
+++ packages/o/oof/xmake.lua
@@ -1,12 +1,12 @@
 package("oof")
     set_kind("library", {headeronly = true})
     set_homepage("https://github.com/s9w/oof")
-    set_description("C and C++ utilities for cross-platform console roguelike game creation")
+    set_description("Convenient, high-performance RGB color and position control for console output")
 
     add_urls("https://github.com/s9w/oof.git")
     add_versions("c7b7c10", "c7b7c1011c2fc8c4f0dd2d7c40a626e9ce965e33")
 
     on_install(function (package)
-        os.cp("oof.h", package:installdir("include/oof"))
+        os.cp("oof.h", package:installdir("include"))
     end)
 
--- packages/r/rlutil/xmake.lua
+++ packages/r/rlutil/xmake.lua
@@ -7,6 +7,6 @@ package("rlutil")
     add_versions("821fdca", "821fdca0191b314ee07b0fad2abe4ea973e45575")
 
     on_install(function (package)
-        os.cp("rlutil.h", package:installdir("include/rlutil"))
+        os.cp("rlutil.h", package:installdir("include"))
     end)
 
