4b5503d5e01ffb4adaac1518f13b0de5ef29203a
swordfatih <accfldekur@gmail.com>
2022-03-29 21:02:08 +0200

    fix libs

--- packages/o/oof/xmake.lua
+++ packages/o/oof/xmake.lua
@@ -7,6 +7,6 @@ package("oof")
     add_versions("c7b7c10", "c7b7c1011c2fc8c4f0dd2d7c40a626e9ce965e33")
 
     on_install(function (package)
-        os.cp("oof.h", package:installdir("include"))
+        os.cp("oof.h", package:installdir("include/oof"))
     end)
 
