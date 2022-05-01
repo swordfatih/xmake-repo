797c9210880ebe40e8a15a3968c650209a24f34d
swordfatih <accfldekur@gmail.com>
2022-05-01 16:09:21 +0200

    fix sfml

--- packages/s/sfml-nocmake/xmake.lua
+++ packages/s/sfml-nocmake/xmake.lua
@@ -112,7 +112,7 @@ package("sfml-nocmake")
                 if graphics then
                     add_files("src/SFML/Graphics/*.cpp")
                 end
-        ]], arch, tostring(graphics), tostring(window), tostring(audio), tostring(network))
+        ]], arch, package:config("graphics"), package:config("window"), package:config("audio"), package:config("network"))
 
         io.writefile("xmake.lua", xmake_lua);
         import("package.tools.xmake").install(package, configs)
