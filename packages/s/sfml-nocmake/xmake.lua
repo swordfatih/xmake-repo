28773782b26980ae9563b327ef72570d2ddfb8db
swordfatih <accfldekur@gmail.com>
2022-05-01 15:36:49 +0200

    fix sfml

--- packages/s/sfml-nocmake/xmake.lua
+++ packages/s/sfml-nocmake/xmake.lua
@@ -15,66 +15,64 @@ package("sfml-nocmake")
             if is_arch("x86", "i386") then
                 arch = "x86"
             end
-
+            
             target("sfml")
                 -- Meta
                 set_languages("c++17")
                 set_kind("static")
-
+            
                 -- Rules
                 add_rules("mode.debug", "mode.release")
                 
                 -- Defines
                 add_defines("SFML_STATIC")
-
-                if is_os("windows") then
+            
+                if is_host("windows") then
                     add_defines("UNICODE")
                 end
                 
                 -- Link libraries
                 add_links("freetype", "openal32", "FLAC", "vorbisenc", "vorbisfile", "vorbis", "ogg")
                 add_linkdirs("extlibs/bin/" .. arch)
-
+            
                 local plat = "mingw"
                 if is_plat("windows") then
                     plat = "msvc-universal"
                 end
-
+            
                 add_linkdirs("extlibs/libs-" .. plat .. "/" .. arch)
-
+            
                 -- Dependencies
-                if is_plat("linux") then
+                if is_host("linux") then
                     add_deps("libx11", "libxrandr", "freetype", "eudev", "libogg", "libflac", "libvorbis", "openal-soft")
-                end
-                
-                if is_os("windows") then
+                elseif is_host("windows") then
                     add_syslinks("opengl32", "gdi32", "user32", "advapi32", "ws2_32", "winmm")
                 end
-
+            
                 -- Include libraries headers
                 add_includedirs("extlibs/headers", "extlibs/headers/AL", "extlibs/headers/freetype2", "extlibs/headers/glad/include", "extlibs/headers/mingw", "extlibs/headers/minimp3", "extlibs/headers/ogg", "extlibs/headers/stb_image", "extlibs/headers/vorbis", "extlibs/headers/vulkan")
                     
                 -- Source code
                 add_includedirs("include", "src")
-
+            
                 -- Implementation files
                 local os = "Win32"
-                if is_os("linux") then
+                if is_host("linux") then
                     os = "Unix"
                 end
-
+            
                 -- Source code
                 add_files("src/SFML/System/" .. os .. "/*.cpp")
                 add_files("src/SFML/System/*.cpp")
-
+            
                 add_files("src/SFML/Network/" .. os .. "/*.cpp")
                 add_files("src/SFML/Network/*.cpp")
-
+            
                 add_files("src/SFML/Audio/*.cpp") 
-
+            
                 add_files("src/SFML/Window/" .. os .. "/*.cpp")
                 add_files("src/SFML/Window/*.cpp")
-
+            
                 add_files("src/SFML/Graphics/*.cpp")
         ]])
 
