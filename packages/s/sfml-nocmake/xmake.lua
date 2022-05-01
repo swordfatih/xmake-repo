36fdc6f0b31491d7795aecde08387c2bc4927f84
swordfatih <accfldekur@gmail.com>
2022-05-01 15:45:30 +0200

    fix sfml

--- packages/s/sfml-nocmake/xmake.lua
+++ packages/s/sfml-nocmake/xmake.lua
@@ -86,5 +86,4 @@ package("sfml-nocmake")
         import("package.tools.xmake").install(package, configs)
 
         os.cp("include/SFML", package:installdir("include"))
-        os.cp("src/SFML", package:installdir("src"))
     end)
