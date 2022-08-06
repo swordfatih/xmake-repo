3c2512a3f492eb9b8acebc3be3c2c85c84a64238
swordfatih <accfldekur@gmail.com>
2022-08-06 21:44:42 +0200

    add thor versions

--- packages/t/thor/xmake.lua
+++ packages/t/thor/xmake.lua
@@ -4,6 +4,9 @@ package("thor")
     set_description("SFML Extension with various game programming features, like particles, animations, vector operations")
 
     add_urls("https://github.com/Bromeon/Thor.git")
+
+    add_versions("2.1", "3e320cb52606f0b44fd9d2bb272b3cb6d01d7f20")
+    add_versions("2.0", "84f4821e49036dc71421e3f628386d2136d96756")
   
     -- Load
     on_load(function (package)
