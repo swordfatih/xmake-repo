0cd676ca326b1fd3bcf79a726fcbea0710430920
swordfatih <accfldekur@gmail.com>
2022-08-06 15:20:44 +0200

    add stduuid package

---
+++ packages/s/stduuid/xmake.lua
@@ -0,0 +1,11 @@
+package("stduuid")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/mariusbancila/stduuid")
+    set_description("A C++17 cross-platform implementation for UUIDs")
+
+    add_urls("https://github.com/mariusbancila/stduuid.git")
+    add_versions("master", "3afe7193facd5d674de709fccc44d5055e144d7a")
+
+    on_install(function (package)
+        os.cp("include", package:installdir("include"))
+    end)
