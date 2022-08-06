5c3c2e9e2f5f4d2f3f21e9ec88ca645d949d9883
swordfatih <accfldekur@gmail.com>
2022-08-06 16:25:46 +0200

    remove versions for cnroom and stduuid

--- packages/c/cnroom/xmake.lua
+++ packages/c/cnroom/xmake.lua
@@ -4,7 +4,6 @@ package("cnroom")
     set_description("Header-only file based key-value database library using C++")
 
     add_urls("https://github.com/swordfatih/CNRoom.git")
-    add_versions("master", "ebf42a52eba3ca7719eef0f8480dac498f3ffc96")
 
     on_install(function (package)
         os.cp("include/CNRoom", package:installdir("include"))
