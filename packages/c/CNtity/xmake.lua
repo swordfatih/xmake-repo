7fd4be526166291b8ce92da9ef79bf8126b1c85a
swordfatih <accfldekur@gmail.com>
2022-08-06 14:59:52 +0200

    add cntity package

---
+++ packages/c/CNtity/xmake.lua
@@ -0,0 +1,12 @@
+package("cntity")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/swordfatih/CNtity")
+    set_description("Helper library using C++ for an Entity Component System implementation")
+
+    add_urls("https://github.com/swordfatih/CNtity.git")
+    add_versions("dc52ec", "dc52e9680b68b12e003c36842b12c6aadde99eac")
+
+    on_install(function (package)
+        os.cp("cntity", package:installdir("include"))
+    end)
+
