5686a1498c3c24951d7dd4110baa40209122462e
swordfatih <accfldekur@gmail.com>
2022-08-06 15:33:06 +0200

    fix directory name for stduuid

--- packages/s/stduuid/xmake.lua
+++ packages/s/stduuid/xmake.lua
@@ -7,5 +7,5 @@ package("stduuid")
     add_versions("master", "3afe7193facd5d674de709fccc44d5055e144d7a")
 
     on_install(function (package)
-        os.cp("$(projectdir)/**.h", package:installdir("include"))
+        os.cp("include/*.h", package:installdir("include"))
     end)
