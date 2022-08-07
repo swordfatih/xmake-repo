3b1e2d3650e43b10b212985827012364284181a2
swordfatih <accfldekur@gmail.com>
2022-08-07 21:50:08 +0200

    cngui & thor: add deps

--- packages/c/cngui/xmake.lua
+++ packages/c/cngui/xmake.lua
@@ -4,6 +4,7 @@ package("cngui")
     set_description("GUI Library using SFML and C++")
 
     add_urls("https://github.com/swordfatih/CNGui.git")
+    add_deps("sfml-nocmake 2.6.0")
   
     -- Load
     on_load(function (package)
@@ -15,7 +16,7 @@ package("cngui")
     on_install(function (package)
         local xmake_lua = [[
             add_repositories("xrepo_fatih https://github.com/swordfatih/xmake-repo.git main")
-            add_requires("sfml-nocmake 2.6.0", { configs = { audio = false, network = false } }) 
+            add_requires("sfml-nocmake 2.6.0") 
 
             target("cngui")
                 -- Meta
--- packages/t/thor/xmake.lua
+++ packages/t/thor/xmake.lua
@@ -4,6 +4,7 @@ package("thor")
     set_description("SFML Extension with various game programming features, like particles, animations, vector operations")
 
     add_urls("https://github.com/Bromeon/Thor.git")
+    add_deps("sfml-nocmake 2.6.0")
 
     add_versions("2.1", "3e320cb52606f0b44fd9d2bb272b3cb6d01d7f20")
     add_versions("2.0", "84f4821e49036dc71421e3f628386d2136d96756")
@@ -18,7 +19,7 @@ package("thor")
     on_install(function (package)
         local xmake_lua = [[
             add_repositories("xrepo_fatih https://github.com/swordfatih/xmake-repo.git main")
-            add_requires("sfml-nocmake 2.6.0", { configs = { network = false } }) 
+            add_requires("sfml-nocmake 2.6.0") 
             add_requires("aurora master") 
 
             target("thor")
