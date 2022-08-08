88e79c22b778fd1fc3d58c54b4d45179227ad88a
swordfatih <accfldekur@gmail.com>
2022-08-08 20:28:29 +0200

    thor: add fork

--- packages/t/thor/xmake.lua
+++ packages/t/thor/xmake.lua
@@ -3,11 +3,14 @@ package("thor")
     set_homepage("https://bromeon.ch/libraries/thor/")
     set_description("SFML Extension with various game programming features, like particles, animations, vector operations")
 
-    add_urls("https://github.com/Bromeon/Thor.git")
+    add_urls("https://github.com/Bromeon/Thor.git", {alias = "bromeon"})
+    add_urls("https://github.com/swordfatih/Thor.git", {alias = "swordfatih"})
+    
     add_deps("sfml-nocmake 2.6.0")
 
-    add_versions("2.1", "3e320cb52606f0b44fd9d2bb272b3cb6d01d7f20")
-    add_versions("2.0", "84f4821e49036dc71421e3f628386d2136d96756")
+    add_versions("bromeon:2.1", "3e320cb52606f0b44fd9d2bb272b3cb6d01d7f20")
+    add_versions("swordfatih:2.1", "458d213151f2b305820e98c6b3e96be67d4ae953")
+    add_versions("bromeon:2.0", "84f4821e49036dc71421e3f628386d2136d96756")
 
     -- Linking
     add_links("thor")
