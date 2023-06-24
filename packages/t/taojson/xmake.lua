5d0e444dbe316a8785d52c17ee0d096b3b30d45e
swordfatih <accfldekur@gmail.com>
2023-06-24 12:49:42 +0200

    feat: added taojson

---
+++ packages/t/taojson/xmake.lua
@@ -0,0 +1,12 @@
+package("taojson")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/taocpp/json")
+    set_description("taoJSON is a C++ header-only JSON library.")
+
+    add_urls("https://github.com/taocpp/json.git")
+    add_versions("main", "330129305f15fbfba5e0716db25e245f0b4d8b0f")
+
+    on_install(function (package)
+        os.cp("include/taojson", package:installdir("include"))
+    end)
+
